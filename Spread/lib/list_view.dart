665378480df1569be4327862c3c488c66b43969e
Phi <113067586+tephira@users.noreply.github.com>
2022-09-26 08:09:46 -0700

    Changed ListView
    ListView now uses an item builder.
    
    This should allow item entries to be made based on stored data

--- Spread/lib/list_view.dart
+++ Spread/lib/list_view.dart
@@ -5,30 +5,24 @@ class launchListView extends StatelessWidget {
   //final ListView myListView;
   //const launchView({super.key, required this.myListView})
 
+  final List<String> entries = <String>["First placeholder",
+    "Second placeholder", "Third placeholder"];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return ListView(
+    return ListView.separated(
       padding: const EdgeInsets.all(8) ,
-      children: <Widget>[
-        Container(
-          height: 30,
+      itemCount: entries.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 50,
           color: Colors.blue,
-          child: const Center(child: Text('Test 1')),
-        ),
-        Container(
-          height: 30,
-          color: Colors.blue,
-          child: const Center(child: Text('Test 2')),
-        ),
-        Container(
-          height: 30,
-          color: Colors.blue,
-          child: const Center(child: Text('Test 3')),
-        )
+          child: Center(child: Text(entries[index]),),
+        );
+      },
 
-      ],
-
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
 
     throw UnimplementedError();
