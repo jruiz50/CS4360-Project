3c45ad0de9e3c4714554a91111db3c2e30b59771
JA2016 <joaquin.g.arroyo@gmail.com>
2022-10-02 22:13:47 -0600

    removed unnecessary comments and added aditional permission in android manifest

--- Spread/lib/mapPage/mapView.dart
+++ Spread/lib/mapPage/mapView.dart
@@ -18,9 +18,7 @@ class UserCoordinate extends State<MapWidget> {
     // Test if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
+      // Location services are not enabled, print a message to the console
       print('Location services are disabled.');
     }
 
@@ -28,17 +26,13 @@ class UserCoordinate extends State<MapWidget> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
+        // Permissions are denied, print a message to the console
         print('Location permissions are denied');
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
+      // Permissions are denied forever.
       print(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
@@ -55,23 +49,7 @@ class UserCoordinate extends State<MapWidget> {
 
     return Scaffold(
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
