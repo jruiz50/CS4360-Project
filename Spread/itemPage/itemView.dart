f84f56c90a96bffbf3340c04bc9c7636ead6e211
Phi <113067586+tephira@users.noreply.github.com>
2022-10-08 18:26:21 -0600

    Created placeholder profile cards
    Profile has placeholder for pictures that a user can set potentially.
    
    Also comes with 4 card tiles meant for menus, favorites, friends, and settings.
    
    Will have to remove settings on bottom nav as a result

---
+++ Spread/itemPage/itemView.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../userPage/userView.dart';
+
+// This'll probably get combined with "foodItemObject" because it's going to use all the same info
+
+class itemView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Scaffold(
+      appBar: ItemAppBar(
+        appBar: AppBar(),
+      ),
+
+      body: Center(
+        child: Column(
+
+            children: <Widget>[
+              Icon(Icons.fastfood, size: 400,), // Absolutely a placeholder; change it Image()
+              Align(
+                alignment: Alignment.topLeft,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: <Row>[
+                    Row(children: <Icon>[
+                      Icon(Icons.star, color: Colors.yellow,size: 50,),
+                      Icon(Icons.star, color: Colors.yellow,size: 50,),
+                      Icon(Icons.star, color: Colors.yellow,size: 50,),
+                      Icon(Icons.star, color: Colors.yellow,size: 50,),
+                      Icon(Icons.star, color: Colors.yellow,size: 50,),
+                    ],),
+                    Row(children: <Text>[
+                      Text('Item Type')
+                    ],),
+                    Row(children: <Text>[
+                      Text('Description')
+                    ],),
+                    Row(children: <Text>[
+                      Text('Ingredients')
+                    ],),
+                    Row(children: <Text>[
+                      Text('Notable Allergens')
+                    ],)
+
+                  ],
+                )
+              ),
+            ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <TextButton>[
+            TextButton(onPressed: null, child: Text("Save")),
+            TextButton(onPressed: null, child: Text("Route"))
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
+class ItemAppBar extends StatelessWidget implements PreferredSizeWidget {
+
+  final AppBar appBar;
+  const ItemAppBar({super.key, required this.appBar});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.greenAccent,
+      title: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Row>[
+            Row(children: <Text>[
+              Text('Name Placeholder')
+            ],),
+            Row(children: <Text>[
+              Text('Restaraunt Placeholder')
+            ],)
+          ],
+        ),
+    )
+    );
+  }
+
+  @override
+  Size get preferredSize => new Size.fromHeight(appBar.preferredSize.height);
+}
