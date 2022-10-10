fc5df1fca1150aa5fe21782e46c10be9c4c62634
Kyle Brown <kbrow164@msudenver.edu>
2022-10-10 15:19:07 -0600

    Widget test write up In progress

--- Spread/lib/loginPage/loginView.dart
+++ Spread/lib/loginPage/loginView.dart
@@ -30,149 +30,157 @@ class loginView extends StatelessWidget{
 
             alignment: Alignment.center,
             padding: const EdgeInsets.all(20),
-            child: Column(
+            child: SingleChildScrollView(
+              child: Column(
 
-              children:  <Widget>[
-                //Fork & Spoon Image
-                Padding(padding: const EdgeInsets.only(top: 30.0,),
-                  child: Image.asset('lib/loginAssets/forkSpoon.png',
-                    scale: 2.5,
+                children:  <Widget>[
+                  //Fork & Spoon Image
+                  Padding(padding: const EdgeInsets.only(top: 30.0,),
+                    child: Image.asset('lib/loginAssets/forkSpoon.png',
+                      scale: 2.5,
+                    ),
                   ),
-                ),
 
-                //Crave Text
-                Text("Crave",
-                  style: GoogleFonts.tangerine(fontSize: 100),
-                ),
-
-                //Email Field
-                TextField(
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: const BorderSide(color:Colors.black, width: 2.0)
-                      ),
-                      hintText: "Enter Email",
-                      hintStyle: const TextStyle(color: Colors.white),
-                      labelText: "Email",
-                      labelStyle: const TextStyle(
-                          color: Colors.white
-                      )
+                  //Crave Text
+                  Text("Crave",
+                    style: GoogleFonts.tangerine(fontSize: 100),
                   ),
-                ),
 
-                //Password Field
-                const SizedBox(height: 10),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: const BorderSide(color:Colors.black, width: 2.0)
-                      ),
-                      hintText: "Enter Password",
-                      hintStyle: const TextStyle(color: Colors.white),
-                      labelText: "Password",
-                      labelStyle: const TextStyle(
-                          color: Colors.white
-                      )
+                  //Email Field
+                  TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(color:Colors.black, width: 2.0)
+                        ),
+                        hintText: "Enter Email",
+                        hintStyle: const TextStyle(color: Colors.white),
+                        labelText: "Email",
+                        labelStyle: const TextStyle(
+                            color: Colors.white
+                        )
+                    ),
                   ),
-                ),
 
-                //Sign In Button
-                Padding(padding: const EdgeInsets.only(top: 25),
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: (){
-                        Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const MyHomePage(title: "Crave")));
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side:const BorderSide(color: Colors.black,
-                              width: 2.0,
+                  //Password Field
+                  const SizedBox(height: 10),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(color:Colors.black, width: 2.0)
+                        ),
+                        hintText: "Enter Password",
+                        hintStyle: const TextStyle(color: Colors.white),
+                        labelText: "Password",
+                        labelStyle: const TextStyle(
+                            color: Colors.white
+                        )
+                    ),
+                  ),
+
+                  //Sign In Button
+                  Padding(padding: const EdgeInsets.only(top: 25),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: (){
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => const MyHomePage(title: "Crave")));
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side:const BorderSide(color: Colors.black,
+                                width: 2.0,
+                              ),
                             ),
                           ),
                         ),
+                        child: const Text('Sign In'),
                       ),
-                      child: const Text('Sign In'),
                     ),
                   ),
-                ),
 
-                //Create Account Button
-                Padding(padding: const EdgeInsets.only(top: 25),
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: (){
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side:const BorderSide(color: Colors.black,
-                              width: 2.0,
+                  //Create Account Button
+                  Padding(padding: const EdgeInsets.only(top: 25),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: (){
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => const MyHomePage(title: "Crave")));
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side:const BorderSide(color: Colors.black,
+                                width: 2.0,
+                              ),
                             ),
                           ),
                         ),
+                        child: const Text('Create Account'),
                       ),
-                      child: const Text('Create Account'),
                     ),
                   ),
-                ),
 
-                //Guest Button
-                Padding(padding: const EdgeInsets.only(top: 25),
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: (){
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side:const BorderSide(color: Colors.black,
-                              width: 2.0,
+                  //Guest Button
+                  Padding(padding: const EdgeInsets.only(top: 25),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: (){
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => const MyHomePage(title: "Crave")));
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side:const BorderSide(color: Colors.black,
+                                width: 2.0,
+                              ),
                             ),
                           ),
                         ),
+                        child: const Text('Continue as Guest'),
                       ),
-                      child: const Text('Continue as Guest'),
                     ),
                   ),
-                ),
+                  //Google Sign In Button
+                  Padding(padding: const EdgeInsets.only(top: 25),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child:
+                      SignInButton(
+                        Buttons.Google,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: const BorderSide(color: Colors.black,
+                              width: 2.0),
+                        ),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => const MyHomePage(title: "Crave")));
+                        },
+                      ),
+                    ),
+                  ),
 
-          //Google Sign In Button
-          Padding(padding: const EdgeInsets.only(top: 25),
-            child: SizedBox(
-              width: double.infinity,
-              height: 50,
-              child:
-              SignInButton(
-                Buttons.Google,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: const BorderSide(color: Colors.black,
-                      width: 2.0),
-                ),
-                onPressed: () {},
+
+                ],
               ),
-            ),
-          ),
-
-
-              ],
             ),
           ),
         ),
--- Spread/test/widget_test.dart
+++ Spread/test/widget_test.dart
@@ -27,4 +27,13 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+
+
+
+
+
+
+
 }
+
+
