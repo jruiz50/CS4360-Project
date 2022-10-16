f9cff94e22b9b529865378b3012a4f5676caebe8
Juan Ruiz <jruiz50@msudenver.edu>
2022-10-16 14:51:10 -0600

    Icon change on click.  Does it for all though...

--- Spread/lib/searchPage/list_view.dart
+++ Spread/lib/searchPage/list_view.dart
@@ -1,63 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spread/itemPage/itemView.dart';
 
-/*class launchListView extends StatelessWidget {
-
-  //final ListView myListView;
-  //const launchView({super.key, required this.myListView})
-
-  final List<String> entries = <String>["First placeholder",
-    "Second placeholder", "Third placeholder"];
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return ListView.separated(
-      padding: const EdgeInsets.all(8) ,
-      itemCount: entries.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            color: Colors.grey
-          ),
-          height: 50,
-
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  const Icon(Icons.fastfood),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  const Text ('Item name'),
-                  const Text ('Item Rating'),
-                  const Text ('Star Rating'),
-
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  IconButton(onPressed: null, icon: Icon(Icons.star_border))
-                ],
-              )
-            ],
-          ),
-
-        );
-      }, // Use a listtile next time, ya goof!!
-
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
-    );
-
-    throw UnimplementedError();
-  }
-
-}*/
-
 class launchListViewGuest extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -106,7 +49,16 @@ class launchListViewGuest extends StatelessWidget{
 
 }
 
-class launchListTile extends StatelessWidget{
+class launchListTile extends StatefulWidget {
+  const launchListTile({super.key});
+
+  @override
+  State<launchListTile> createState() => _launchListTile();
+}
+
+bool isFav = false; // TODO: isFav will need to be grabbed from db
+
+class _launchListTile extends State<launchListTile>{
 
   final List<String> entries = <String>["First placeholder",
     "Second placeholder", "Third placeholder"];
@@ -120,8 +72,6 @@ class launchListTile extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    Icon showIcon = unfav;
-
     return ListView.builder(
       itemCount: entries.length,
       padding: const EdgeInsets.all(10),
@@ -133,7 +83,14 @@ class launchListTile extends StatelessWidget{
             leading: Icon(Icons.fastfood, size: 30,),
             title: Text('Food Name'),
             subtitle: Text('Item Star Rating'),
-            trailing: IconButton(onPressed: (favorite), icon: showIcon),
+            trailing: IconButton(
+                onPressed: () {
+                  setState(() {
+                    isFav = !isFav;
+                    print(isFav);
+                  });
+                },
+                icon: (isFav) ? fav : unfav),
             onTap: (){
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => itemView()));
@@ -146,24 +103,6 @@ class launchListTile extends StatelessWidget{
     );
 
     throw UnimplementedError();
-  }
-
-  void favorite(){
-
-    if(favIconNum == 0){
-      showIcon = unfav;
-      favIconNum = 1;
-      print('Bro');
-
-    }
-
-    else{
-      showIcon = fav;
-      favIconNum = 0;
-      print('Bruh');
-    }
-
-
   }
   
 }
