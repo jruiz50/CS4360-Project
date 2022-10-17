b38a767052ff428433e89702ba000298677f1c8b
Juan Ruiz <jruiz50@msudenver.edu>
2022-10-17 14:55:36 -0600

    changed formatting of db classes

---
+++ Spread/lib/dbObjects/restaurant.dart
@@ -0,0 +1,37 @@
+class restaurant {
+  final int restaurantID;
+  final int regionID;
+  final String restaurantName;
+  final int menuID;
+  final String coordinates;
+  var tags = <String>[];
+  final double rating;
+
+  restaurant(
+      {required this.restaurantID,
+      required this.regionID,
+      required this.restaurantName,
+      required this.menuID,
+      required this.coordinates,
+      required this.tags,
+      required this.rating});
+
+  Map<String, dynamic> toJson() => {
+        'restaurantID': restaurantID,
+        'regionID': regionID,
+        'restaurantName': restaurantName,
+        'menuID': menuID,
+        'coordinates': coordinates,
+        'tags': tags,
+        'rating': rating,
+      };
+
+  static restaurant fromJson(Map<String, dynamic> json) => restaurant(
+      restaurantID: json['restaurantID'],
+      regionID: json['regionID'],
+      restaurantName: json['restaurantName'],
+      menuID: json['menuID'],
+      coordinates: json['coordinates'],
+      tags: json['tags'],
+      rating: json['rating']);
+}
