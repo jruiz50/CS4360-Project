bacd05d1246e734422909a10a3ee4435b8b80a6f
Kyle Brown <kbrow164@msudenver.edu>
2022-12-03 14:18:13 -0700

    fixed?

--- Spread/lib/mapPage/mapView.dart
+++ Spread/lib/mapPage/mapView.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -75,72 +74,3 @@ class UserCoordinate extends State<MapWidget> {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-
-class MapWidget extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => UserCoordinate();
-
-}
-
-class UserCoordinate extends State<MapWidget> {
-
-  String _locationMessage = "n/a";
-
-  void _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled, print a message to the console
-      print('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, print a message to the console
-        print('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever.
-      print(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    setState(() {
-      _locationMessage = "${position.latitude}, ${position.longitude}";
-    });
-  }
-
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              _locationMessage,
-            ),
-            TextButton(
-              onPressed: _determinePosition,
-              child: Text('Get Location'),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
->>>>>>> 05757a797ea12ef4a9eb9893711b970480e8ccd5
